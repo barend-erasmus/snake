0ea2d40259f84219c0df49f71d32d8b83f00955d
Barend Erasmus <developersworkspace@gmail.com>
2017-11-08 14:32:24 +0200

    adds coverage

--- src/snake.test.ts
+++ src/snake.test.ts
@@ -1,10 +1,112 @@
 import { expect } from 'chai';
 import 'mocha';
 
-import { } from './snake';
+import { checkForRewards, cycleSnakePath, generateRewards } from './snake';
 
-describe('getNewCellState', () => {
-    it('should return 0 given 1 neighbour and live cell', () => {
-       
+describe('generateRewards', () => {
+    it('should return 10 rewards', () => {
+        const rewards: { x: number, y: number }[] = generateRewards([]);
+
+        expect(rewards.length).to.be.eq(10);
+    });
+});
+
+describe('checkForRewards', () => {
+    it('should remove rewards given snake path is in same location', () => {
+        const rewards: { x: number, y: number }[] = checkForRewards([
+            {
+                x: 10,
+                y: 20,
+            },
+        ], [
+                {
+                    x: 10,
+                    y: 20,
+                },
+                {
+                    x: 30,
+                    y: 30,
+                },
+            ]);
+
+        expect(rewards.length).to.be.eq(1);
+    });
+});
+
+describe('cycleSnakePath', () => {
+    it('should move up given direction is up', () => {
+        const snakePath: { x: number, y: number }[] = cycleSnakePath(0, [
+            {
+                x: 10,
+                y: 10,
+            },
+            {
+                x: 10,
+                y: 9,
+            },
+        ]);
+
+        expect(snakePath[0].x).to.be.eq(10);
+        expect(snakePath[0].y).to.be.eq(9);
+
+        expect(snakePath[1].x).to.be.eq(10);
+        expect(snakePath[1].y).to.be.eq(8);
+    });
+
+    it('should move down given direction is down', () => {
+        const snakePath: { x: number, y: number }[] = cycleSnakePath(1, [
+            {
+                x: 10,
+                y: 9,
+            },
+            {
+                x: 10,
+                y: 10,
+            },
+        ]);
+
+        expect(snakePath[0].x).to.be.eq(10);
+        expect(snakePath[0].y).to.be.eq(10);
+
+        expect(snakePath[1].x).to.be.eq(10);
+        expect(snakePath[1].y).to.be.eq(11);
+    });
+
+    it('should move left given direction is left', () => {
+        const snakePath: { x: number, y: number }[] = cycleSnakePath(2, [
+            {
+                x: 10,
+                y: 9,
+            },
+            {
+                x: 10,
+                y: 10,
+            },
+        ]);
+
+        expect(snakePath[0].x).to.be.eq(10);
+        expect(snakePath[0].y).to.be.eq(10);
+
+        expect(snakePath[1].x).to.be.eq(9);
+        expect(snakePath[1].y).to.be.eq(10);
+    });
+
+    it('should move right given direction is right', () => {
+        const snakePath: { x: number, y: number }[] = cycleSnakePath(3, [
+            {
+                x: 10,
+                y: 9,
+            },
+            {
+                x: 10,
+                y: 10,
+            },
+        ]);
+
+        expect(snakePath[0].x).to.be.eq(10);
+        expect(snakePath[0].y).to.be.eq(10);
+
+        expect(snakePath[1].x).to.be.eq(11);
+        expect(snakePath[1].y).to.be.eq(10);
     });
 });
